2a6df601fd144886a62562c3aad8142ffe45d16d
Juliano Laurentino <julianolaurentino@gmail.com>
2025-04-17 18:31:53 -0300

    update files

---
+++ src/transformLoad/main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import sqlite3
+from datetime import datetime
+
+df = pd.read_json(r'D:\Users\Inked\Documents\JULIANO\workspace\VS Code\Python\Python-ws-api\base\data.json')
+
+pd.options.display.max_columns = None
+
+
+# Setar o pandas para mostrar todas as colunas
+pd.options.display.max_columns = None
+
+# Adicionar a coluna _source com um valor fixo
+df['_source'] = "https://lista.mercadolivre.com.br/notebook"
+
+# Adicionar a coluna _data_coleta com a data e hora atuais
+df['_datetime'] = datetime.now()
+
+# Tratar nulos
+df['old_money'] = df['old_money'].fillna('0')
+df['new_money'] = df['new_money'].fillna('0')
+df['reviews_rating_number'] = df['reviews_rating_number'].fillna('0')
+df['reviews_amount'] = df['reviews_amount'].fillna('(0)')
+
+# Garantir que estão como strings antes de usar .str
+df['old_money'] = df['old_money'].astype(str).str.replace('.', '', regex=False)
+df['new_money'] = df['new_money'].astype(str).str.replace('.', '', regex=False)
+df['reviews_amount'] = df['reviews_amount'].astype(str).str.replace('[\(\)]', '', regex=True)
+
+# Converter para números
+df['old_money'] = df['old_money'].astype(float)
+df['new_money'] = df['new_money'].astype(float)
+df['reviews_rating_number'] = df['reviews_rating_number'].astype(float)
+df['reviews_amount'] = df['reviews_amount'].astype(int)
+
+# Tratar os preços como floats e calcular os valores totais
+# Manter apenas produtos com preço entre 1000 e 10000 reais
+df = df[
+    (df['old_money'] >= 1000) & (df['old_money'] <= 10000) &
+    (df['new_money'] >= 1000) & (df['new_money'] <= 10000)
+]
+
+# Conectar ao banco de dados SQLite (ou criar um novo)
+conn = sqlite3.connect(r'D:\Users\Inked\Documents\JULIANO\workspace\VS Code\Python\Python-ws-api\base\mercadolivre.db')
+
+# Salvar o DataFrame no banco de dados SQLite
+df.to_sql('notebook', conn, if_exists='replace', index=False)
+
+# Fechar a conexão com o banco de dados
+conn.close()
